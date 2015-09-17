944cc22a211bf54413b9648e00a8cfb85d1d7fef
TheFurrow <furrow@live.fi; git config --global user.name Furrow>
2015-09-17 15:14:53 +0300

    New testing push

--- register.js
+++ register.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host	: 'localhost',
 	user	: 'root',
-	password: 'test1234',
+	password: '12345',
 	database: 'vsklogin'
 });
 
