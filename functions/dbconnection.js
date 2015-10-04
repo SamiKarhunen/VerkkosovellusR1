0c0eb01e695703a9cf5e5e26279d0c044d46476e
fuzzeq <karhunensami@hotmail.com>
2015-10-04 13:03:36 +0300

    julkaisujuttuja4

--- functions/dbconnection.js
+++ functions/dbconnection.js
@@ -1,6 +1,7 @@
 module.exports = {
-	host	: 'localhost',
-	user	: 'root',
-	password: 'test1234',
+	host	: process.env.OPENSHIFT_MYSQL_DB_HOST,
+	user	: process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+	password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+	port	: process.env.OPENSHIFT_MYSQL_DB_PORT
 	database: 'vsklogin'
 };
