3cedc73e28fcdb2111fb42045c054f6e1750885c
fuzzeq <karhunensami@hotmail.com>
2015-10-04 18:22:36 +0300

    julkaisujuttuja12

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var connection = mysql.createConnection(require('./functions/dbconnection.js'));
 var register = require('./functions/register.js');
 var login = require('./functions/login.js');
 var logout = require('./functions/logout.js');
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.use(express.static('public'));
 
@@ -99,7 +99,7 @@ io.on('connection', function(socket){
 
 
 
-http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
-    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
-    server();
+http.listen(port, function(){
+	console.log("Listening on " + ip);
 });
+
