5856bc365729cbebc9be149cc41bf58c7ef87d27
oh5gzr <ville@goblet.net>
2015-10-05 00:23:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection(require('./functions/dbconnection.js'));
 var register = require('./functions/register.js');
 var login = require('./functions/login.js');
 var logout = require('./functions/logout.js');
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.use(express.static('public'));
